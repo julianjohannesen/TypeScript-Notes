739e76592079cfd80c45963988354bea74c7ca82
Julian Johannesen <julian.johannesen@gmail.com>
2023-02-02 21:30:50 +0000

    type definitions and libraries

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# notes-on-codecademy-ts
-Some notes while going through Codecademy's TypeScript course.
+# Notes on TypeScript
+
+Some notes while watching [TypeScript Essential Training](https://www.linkedin.com/learning/typescript-essential-training-14687057/) on LinkedIn Learning. 
+
+Note that I installed the recommended packages via NPM as they were suggested throughout the progress of the course. So, if you're stumbling upon this repo and want to use it, you'll want to delete the list of packages in package.json, and also package-lock.json. 
---
+++ src/using-3rd-party-libraries.js
@@ -0,0 +1,12 @@
+/* 
+
+You can use almost any library with TS, but you'll need to find the appropriate type definitions package for that library. Here are two places to do that:
+
+- https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types
+- https://www.npmjs.com/search?q=%40types
+
+If you're using a different package manager, you'll need to find the appropriate type definitions package on the appropriate site. 
+
+So, for example, in order to use jQuery with TS, you'll need to install the @types/jquery package. Once that's done, you'll find @types/jquery/index.d.ts in your node_modules directory. The .d stands for 'definitions.' You'll see several other files as well.
+
+*/
